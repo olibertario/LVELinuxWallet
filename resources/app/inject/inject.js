9ee5be45cb4834f4a769b3d844915d6488ba5d1e
1nv4d3r2 <cappelozzi@gmail.com>
2021-10-01 22:46:30 -0300

    LVEwalletLinux commit

---
+++ resources/app/inject/inject.js
@@ -0,0 +1,8 @@
+var data_tracking =  {
+      platform: "linux",
+      appID:"bwIxHHdwi5tiffhLcAD9",
+      url:"https://lvewebwallet.space/",
+      appname:"LVE Windows Wallet"
+    };
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push(data_tracking);(function (w, d, s, l, i) {  w[l] = w[l] || [];  w[l].push({  'gtm.start': new Date().getTime(), event: 'gtm.js', }); let f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? `&l=${l}` : ''; j.async = true; j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`; f.parentNode.insertBefore(j, f); }(window, document, 'script', 'dataLayer', 'GTM-N9XW463'));
